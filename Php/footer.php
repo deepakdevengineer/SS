963928f00f1bdf3591f7e4ec2c1d45ca3ef7ea6e
Mudit jain <muditj214@gmail.com>
2023-12-10 12:37:12 +0530

    changes

---
+++ Php/footer.php
@@ -0,0 +1,191 @@
+<footer class="footer-area">
+      <div class="footer-instagram">
+        <div class="container">
+          <div class="row g-0 instagram-active">
+            <div class="col-2">
+              <div class="footer-insta-item">
+                <a
+                  href="assets/img/others/instagram_post01.png"
+                  class="popup-image"
+                  ><img src="assets/img/others/instagram_post01.png" alt="img"
+                /></a>
+              </div>
+            </div>
+            <div class="col-2">
+              <div class="footer-insta-item">
+                <a
+                  href="assets/img/others/instagram_post02.png"
+                  class="popup-image"
+                  ><img src="assets/img/others/instagram_post02.png" alt="img"
+                /></a>
+              </div>
+            </div>
+            <div class="col-2">
+              <div class="footer-insta-item">
+                <a
+                  href="assets/img/others/instagram_post03.png"
+                  class="popup-image"
+                  ><img src="assets/img/others/instagram_post03.png" alt="img"
+                /></a>
+              </div>
+            </div>
+            <div class="col-2">
+              <div class="footer-insta-item">
+                <a
+                  href="assets/img/others/instagram_post04.png"
+                  class="popup-image"
+                  ><img src="assets/img/others/instagram_post04.png" alt="img"
+                /></a>
+              </div>
+            </div>
+            <div class="col-2">
+              <div class="footer-insta-item">
+                <a
+                  href="assets/img/others/instagram_post05.png"
+                  class="popup-image"
+                  ><img src="assets/img/others/instagram_post05.png" alt="img"
+                /></a>
+              </div>
+            </div>
+            <div class="col-2">
+              <div class="footer-insta-item">
+                <a
+                  href="assets/img/others/instagram_post06.png"
+                  class="popup-image"
+                  ><img src="assets/img/others/instagram_post06.png" alt="img"
+                /></a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="footer-top-wrap">
+        <div class="container">
+          <div class="footer-widgets-wrap">
+            <div class="row">
+              <div class="col-lg-4 col-md-7">
+                <div class="footer-widget">
+                  <div class="footer-about">
+                    <div class="footer-logo logo">
+                      <a href="index.html"
+                        ><img src="assets/img/logo/white_logo.png" alt="Logo"
+                      /></a>
+                    </div>
+                    <div class="footer-text">
+                      <p>
+                        Promoting accessible and sustainable indoor gardening.
+Exceptional customer experiences while contributing positively to the global ecosystem.
+                      </p>
+                    </div>
+                    <div class="footer-social">
+                      <a href="#"><i class="fab fa-facebook-f"></i></a>
+                      <a href="#"><i class="fab fa-twitter"></i></a>
+                      <a href="#"><i class="fab fa-instagram-p"></i></a>
+                      <a href="#"><i class="fab fa-linkedin-in"></i></a>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div class="col-lg-3 col-md-5 col-sm-6">
+                <div class="footer-widget">
+                  <h4 class="fw-title">About Us</h4>
+                  <ul class="list-wrap">
+                    <li><a href="#">About Company</a></li>
+                    <li><a href="#">Affiliate Program</a></li>
+                    <li><a href="shop.html">Our Shop</a></li>
+                    <li><a href="#">Price & Plans</a></li>
+                  </ul>
+                </div>
+              </div>
+              <div class="col-lg-2 col-md-5 col-sm-6">
+                <div class="footer-widget">
+                  <h4 class="fw-title">Support</h4>
+                  <ul class="list-wrap">
+                    <li><a href="#">Knowledge Base</a></li>
+                    <li><a href="blog.html">Blog</a></li>
+                    <li><a href="#">FAQ</a></li>
+                    <li><a href="#">Team</a></li>
+                    <li><a href="contact.html">Contact</a></li>
+                  </ul>
+                </div>
+              </div>
+              <div class="col-lg-3 col-md-5">
+                <div class="footer-widget">
+                  <h4 class="fw-title">CONTACT US</h4>
+                  <div class="footer-contact-wrap">
+                    <p>Thane, Mumbai , India 400601</p>
+                    <ul class="list-wrap">
+                      <li class="phone">
+                        <i class="fas fa-phone"></i> +918076406706
+                      </li>
+                      <li class="mail">
+                        <i class="fas fa-envelope"></i> info.agriqulture@gmail.com
+                      </li>
+                      <li class="website">
+                        <i class="fas fa-globe"></i> www.SSAgriqulture.com
+                      </li>
+                    </ul>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="footer-shape one">
+          <img
+            src="assets/img/others/footer_shape01.png"
+            alt="img"
+            class="wow fadeInLeft"
+            data-wow-delay=".3s"
+            data-wow-duration="1s"
+          />
+        </div>
+        <div class="footer-shape two">
+          <img
+            src="assets/img/others/footer_shape02.png"
+            alt="img"
+            class="wow fadeInRight"
+            data-wow-delay=".3s"
+            data-wow-duration="1s"
+          />
+        </div>
+      </div>
+      <div class="copyright-wrap">
+        <div class="container">
+          <div class="row align-items-center">
+            <div class="col-md-7">
+              <div class="copyright-text">
+                <p>Copyright © 2023 SS Agriqulture All Rights Reserved.</p>
+              </div>
+            </div>
+            <div class="col-md-5">
+              <div class="payment-card text-center text-md-end">
+                <img src="assets/img/others/card_img.png" alt="card" />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </footer>
+    <!-- Footer-area-end -->
+
+    <!-- JS here -->
+    <script src="assets/js/vendor/jquery-3.6.0.min.js"></script>
+    <script src="assets/js/bootstrap.min.js"></script>
+    <script src="assets/js/isotope.pkgd.min.js"></script>
+    <script src="assets/js/imagesloaded.pkgd.min.js"></script>
+    <script src="assets/js/jquery.magnific-popup.min.js"></script>
+    <script src="assets/js/jquery.paroller.min.js"></script>
+    <script src="assets/js/jquery.easypiechart.min.js"></script>
+    <script src="assets/js/jquery.inview.min.js"></script>
+    <script src="assets/js/jquery.easing.js"></script>
+    <script src="assets/js/jquery-ui.min.js"></script>
+    <script src="assets/js/jarallax.min.js"></script>
+    <script src="assets/js/slick.min.js"></script>
+    <script src="assets/js/validator.js"></script>
+    <script src="assets/js/ajax-form.js"></script>
+    <script src="assets/js/wow.min.js"></script>
+    <script src="assets/js/main.js"></script>
+    <script src="assets/js/newfe.js"></script>
+    </body>
+    </html>
