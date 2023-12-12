51e66e9349f217521e8945d31d4c1f67a1f33811
Mudit jain <muditj214@gmail.com>
2023-12-12 23:57:36 +0530

    changes

--- Php/contact.php
+++ Php/contact.php
@@ -1,18 +1,40 @@
 <?php
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $name = $_POST['name'];
-    $email = $_POST['email'];
-    $message = $_POST['text'];
-    
-    $to = 'muditj214@gmail.com'; // Replace with your email address
-    $subject = 'New Feedback from Contact Form';
-    $body = "Name: $name\nEmail: $email\nMessage: $message";
-    $headers = 'From: ' . $email;
+// Include PHPMailer autoload file
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
 
-    if (mail($to, $subject, $body, $headers)) {
+require 'php mailer/src/Exception.php';
+require 'php mailer/src/PHPMailer.php';
+require 'php mailer/src/SMTP.php';
+
+if (isset($_POST["submit"])) {
+    
+    // PHPMailer configuration
+    $mail = new PHPMailer(true);
+    $mail->isSMTP();
+    $mail->Host = 'smtp.gmail.com'; // Your SMTP host (for Gmail, use smtp.gmail.com)
+    $mail->SMTPAuth = true;
+    $mail->Username = 'mjmjajaj715@gmail.com'; // Your Gmail address
+    $mail->Password = ' rgna azkx zlha lykr'; // Your Gmail password or app password
+    $mail->SMTPSecure = 'ssl'; // Enable TLS encryption, `ssl` also accepted
+    $mail->Port = 465; // TCP port to connect to for Gmail SMTP is 465
+
+    $mail->setFrom('mjmjajaj715@gmail.com');
+    $mail->addAddress($_POST["email"]); 
+    // Replace with your email address
+
+
+    $mail->Subject = $_POST["Name"];
+    $mail->Body = $_POST["Comment"];
+
+    $mail->send();
+    try {
+        // Send mail
         echo '<p>Your message has been sent successfully!</p>';
-    } else {
+    } catch (Exception $e) {
         echo '<p>Sorry, there was an error sending your message. Please try again later.</p>';
+        // Uncomment the line below for debugging (do not use in production)
+        // echo 'Mailer Error: ' . $mail->ErrorInfo;
     }
 }
 ?>
